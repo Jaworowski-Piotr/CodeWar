6cdc3cc4b12be2167c166743e4d9129907495a54
Piotr Jaworowski <jaworowski.piotr.marek@gmail.com>
2022-09-27 19:08:44 +0200

    Dodano plik z zadaniem Stop gninnipSMy sdroW!

--- Shortest word.py
+++ Shortest word.py
@@ -1,10 +1,9 @@
 def find_short(s):
-    new_list = s.split(" ")
-    print(new_list)
-    numerki = []
-    for word in new_list:
-        numerki.append(len(word))
-    l = min(numerki)
+    spilt_list = s.split(" ")
+    new_list = []
+    for word in spilt_list:
+        new_list.append(len(word))
+    l = min(new_list)
     return l
 
 s = "bitcoin take over the world maybe who knows perhaps"
