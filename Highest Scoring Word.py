bdc85d32c852cac96c7deaf9e54bf971e2b7e390
Piotr Jaworowski <jaworowski.piotr.marek@gmail.com>
2022-09-28 13:04:02 +0200

    Dodano komentarze do rozwiązania zadania Highest Scoring Word

--- Highest Scoring Word.py
+++ Highest Scoring Word.py
@@ -1,9 +1,9 @@
 def high(s):
     alpha = {
         "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10,
-        "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "r": 17, "s": 18, "t": 19,
-        "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25
-    }
+        "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20,
+        "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26
+    }    #można było urzyć funkcji ord
     values = []
     words = s.split(" ")
     for word in words:
@@ -12,6 +12,15 @@ def high(s):
             value_of_word += alpha[letter]
         print(value_of_word)
         values.append(value_of_word)
-    return words[values.index(max(values))]
-s= "what time are we climbing up the volcano"
+    return str(words[values.index(max(values))])
+s= "zrczzrmwfy ytjun twgm ulpbkvldk yljzqsqicg mwt cibsqpplat nmcgcflmr diomiozfom mtmthstfa hxv fwcy nzkfytzg glvu dhwa ewxfgnayg"
 print(high(s))
+
+####Najciekawsze rozwiązanie####
+# def high(x):
+#     words=x.split(' ')
+#     list = []
+#     for i in words:
+#         scores = [sum([ord(char) - 96 for char in i])]
+#         list.append(scores)
+#     return words[list.index(max(list))]
