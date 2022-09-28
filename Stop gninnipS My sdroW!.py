87c389483a8252864933e173f00660387697b3d9
Piotr Jaworowski <jaworowski.piotr.marek@gmail.com>
2022-09-28 11:29:38 +0200

    Rozwiązano zadanie Stop gninnipS My sdroW

--- Stop gninnipS My sdroW!.py
+++ Stop gninnipS My sdroW!.py
@@ -1,3 +1,13 @@
 def spin_words(sentence):
-    # Your code goes here
+    temp = []
+    for word in sentence.split(" "):
+        if len(word) >= 5:
+            temp.append(word[::-1])
+        else:
+            temp.append(word)
+    print(" ".join(temp))
     return None
+
+
+sentence = "Welcome to a jungle"
+spin_words(sentence)
