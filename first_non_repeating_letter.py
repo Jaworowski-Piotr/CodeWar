57b221939e6ffd2f84cfbe015243aee50d257e87
Piotr Jaworowski <jaworowski.piotr.marek@gmail.com>
2022-09-29 16:02:46 +0200

    Dodano plik z rozwiązaniem zadania Frist non repeating letter

--- first_non_repeating_letter.py
+++ first_non_repeating_letter.py
@@ -1,4 +1,17 @@
 def first_non_repeating_letter(string):
-    #your code here
+    temp = string.lower()
+    all_repeat = False
+    if len(string) == 0:
+        return ""
+    for i, char in enumerate(temp):
+        if temp.count(char) <= 1:
+            return string[i]
+        elif temp.count(char) > 1:
+            all_repeat = True
+        else:
+            all_repeat = False
+    if all_repeat:
+        return None
 
-first_non_repeating_letter('stress')
+
+print(first_non_repeating_letter('sttreess'))
