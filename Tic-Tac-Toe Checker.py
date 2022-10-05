a5d0e297e421de867d097323fcbf4f6e5988231a
Piotr Jaworowski <jaworowski.piotr.marek@gmail.com>
2022-10-05 17:30:28 +0200

    Dodano poprawione rozwiÄazanie zadania Tic Tac Toe Checker

--- Tic-Tac-Toe Checker.py
+++ Tic-Tac-Toe Checker.py
@@ -9,16 +9,29 @@ def win_check(tablica, znak):
             (tablica[2][2] == znak and tablica[1][1] == znak and tablica[0][0] == znak))  # diagonal
 
 def is_solved(board):
-    if 0 in board:
-        return "Not finished yet", -1
-    elif win_check(board, 1):
-        return "Gracz X wygrał", 1
+
+    if win_check(board, 1):
+        return 1
     elif win_check(board, 2):
-        return "Gracz O wygrał", 2
+        return 2
     else:
-        return "Nikt nie wygrał", 0
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == 0:
+                    return -1
+                    break
+        return 0
 
 
 print(is_solved(board=[[0, 0, 1],
                        [0, 1, 2],
                        [2, 1, 0]]))
+
+#Inne ciekawe rozwiązanie
+# import itertools
+#
+# def isSolved(board):
+#     b = list(itertools.chain(*board))
+#     for p, q, r in [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]:
+#         if b[p] == b[q] == b[r] != 0: return b[p]
+#     return 0 if sum(b) == 13 else -1
